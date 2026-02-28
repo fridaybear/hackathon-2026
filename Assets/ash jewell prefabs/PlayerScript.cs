f25f05b85dcaf0d88d83dfcf242f497682f14747
fridaybear <68036166+fridaybear@users.noreply.github.com>
2026-02-28 22:00:06 +0000

    uploaded bulk of script for first scene to work

--- Assets/ash jewell prefabs/PlayerScript.cs
+++ Assets/ash jewell prefabs/PlayerScript.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int health=4;
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            SceneManager.LoadScene("BadEnding");
+        }
     }
 }
--- Assets/ash jewell prefabs/Timer.cs
+++ Assets/ash jewell prefabs/Timer.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Countdown;
+    public GameObject Player;
+    public TextMeshProUGUI text;
+    public string nextScene;
+    private PlayerScript player_script;
 
-    // Update is called once per frame
+    void OnEnable()
+    {
+        player_script = Player.GetComponent<PlayerScript>();
+        Debug.Log("countdown started");
+    }
     void Update()
     {
-        
+        if (Countdown > 0)
+        {
+            text.text = Countdown.ToString("F1");
+            Debug.Log(Countdown);
+            Countdown -= Time.deltaTime;
+        }
+        else
+        {
+            text.text = "you failed";
+            Debug.Log("you failed to escape it");
+            player_script.health -= 1;
+            SceneManager.LoadScene(nextScene);
+        }
     }
 }
