e17aed5ba5777022876244c9ad00308fa5f27eb0
fridaybear <68036166+fridaybear@users.noreply.github.com>
2026-03-01 02:27:47 +0000

    added scripts and more testing and stuff

--- Assets/ash jewell prefabs/spawnTree.cs
+++ Assets/ash jewell prefabs/spawnTree.cs
@@ -2,24 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class combination_script : MonoBehaviour
+public class spawnTree : MonoBehaviour
 {
-
+    public GameObject tree;
     public GameObject combo_obj;
     public GameObject parent_obj;
     public GameObject output_obj;
 
+    private GameObject obj;
+
+    private Animation anim;
+
     private Vector3 position;
     void OnCollisionEnter(Collision collision)
     {
-        if (combo_obj.name == collision.gameObject.name)
+        anim = tree.GetComponent<Animation>();
+
+        if (combo_obj.name == collision.gameObject.name && parent_obj.name != "seed")
         {
             position = parent_obj.transform.position;
             Destroy(collision.gameObject);
             Destroy(parent_obj);
             GameObject newobj = Instantiate(output_obj, position + new Vector3(0,10,0), Quaternion.identity);
+            obj = newobj;
         }
 
+        if (collision.gameObject.name == "collisionObj" && parent_obj.name == "seed")
+        {
+            Destroy(collision.gameObject);
+            Destroy(obj);
+            anim.Play();
+        }
     }
 
 }
---
+++ Assets/ash jewell prefabs/treescript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class treescript : MonoBehaviour
+{
+
+    public GameObject tree;
+    private Animation anim;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ran");
+        anim = tree.GetComponent<Animation>();
+        if (collision.gameObject.name == "collisionObj")
+        {
+            anim.Play();
+            Destroy(collision.gameObject);
+        }
+
+    }
+}
